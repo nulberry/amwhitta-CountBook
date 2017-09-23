87ccfc5ca057c9d6a4bec5a2da68187934e858de
nulberry <andrea.whittaker95@gmail.com>
2017-09-22 18:12:11 -0600

    minor changes

---
+++ app/src/main/java/com/example/amwhitta_countbook/Counter.java
@@ -0,0 +1,50 @@
+package com.example.amwhitta_countbook;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Andrea on 15-09-17.
+ */
+
+public class Counter {
+    private String name;
+    private String date;
+    private int initial;
+    private int current;
+    private String comment;
+    private SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+    // CONSTRUCTOR
+    public Counter(String name, int initial){
+        this.name = name;
+        setDate();
+        this.initial = initial;
+        current = initial;
+        comment = "";
+
+    }
+
+    // BEHAVIOURS
+    public void increment(){ ++current; }
+    public void decrement(){ --current; }
+    public void reset(){ current = initial; }
+
+    // GETTERS
+    public String getName(){ return name; }
+    public String getDate() { return date; }
+    public int getInitial() { return initial; }
+    public int getCurrent() { return current; }
+    public String getComment() { return comment; }
+
+    // SETTERS
+    public void setName(String name){ this.name = name; }
+    public void setInitial(int initial){ this.initial = initial; }
+    public void setCurrent(int current){ this.current = current; }
+    public void setComment(String comment){ this.comment = comment; }
+    public void setDate() {
+        Date temp = new Date();
+        date = formatter.format(temp);
+    }
+
+}
--- app/src/main/java/com/example/amwhitta_countbook/MainActivity.java
+++ app/src/main/java/com/example/amwhitta_countbook/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.amwhitta_countbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView counterList;
+    private EditText counterNumber;
+
+    private ArrayList<Counter> counters = new ArrayList<Counter>();
+    private ArrayAdapter<Counter> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }

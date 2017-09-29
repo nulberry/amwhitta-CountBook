0fc7b63b4e0bb2705cdf0576dc4ecac18ca179bf
nulberry <andrea.whittaker95@gmail.com>
2017-09-29 14:42:40 -0600

    linking activities (has errors)

--- app/src/main/java/com/example/amwhitta_countbook/MainActivity.java
+++ app/src/main/java/com/example/amwhitta_countbook/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.amwhitta_countbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -10,6 +14,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String FILENAME = "file.sav";
     private ListView counterList;
     private EditText counterNumber;
 
@@ -23,6 +28,34 @@ public class MainActivity extends AppCompatActivity {
 
         counterList = (ListView) findViewById(R.id.listViewCounters);
         counterNumber = (EditText) findViewById(R.id.editTextNumCounters);
+        Button newCounter = (Button) findViewById(R.id.buttonNewCounter);
+
+        newCounter.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewCounterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        counterList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this, CounterActivity.class);
+                intent.putExtra("Counter", counters.get(i));
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //loadFromFile();
+    }
+
+    @Override
+    protected void onListItemClick() {
+
     }
 
 
